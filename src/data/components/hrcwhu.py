bc49cf81cc31688f3ad350d1319acb9232a381c3
caixiaoshun <3038523973@qq.com>
2024-08-24 16:10:49 +0800

    对于eval_hrcwhu,定量结果，指标低的放前面，指标高的放后面，定量结果：最高的指标结果标粗，所有数字居中，保留小数点后四位，对于定性结果，方法名称大写，好的效果放前面，列名：Input, Label, xxx

--- src/data/components/hrcwhu.py
+++ src/data/components/hrcwhu.py
@@ -9,7 +9,7 @@ from torch.utils.data import Dataset
 class HRCWHU(Dataset):
     METAINFO = dict(
         classes=('clear sky', 'cloud'),
-        palette=((255, 255, 255),(128, 192, 128)),
+        palette=((128, 192, 128),(255, 255, 255)),
         img_size=(3, 256, 256),  # C, H, W
         ann_size=(256, 256),  # C, H, W
         train_size=120,
--- src/eval/eval_on_hrc_whu.py
+++ src/eval/eval_on_hrc_whu.py
@@ -6,19 +6,14 @@ import csv
 from PIL import Image, ImageDraw, ImageFont
 import numpy as np
 import torchvision
-from rich.console import Console
-import matplotlib.pyplot as plt
-from torchmetrics.wrappers import ClasswiseWrapper
-from torchmetrics import MeanMetric, MetricCollection
+from torchmetrics import MetricCollection
 from torchmetrics.classification import MulticlassAccuracy, F1Score, Precision, Recall
 from torchmetrics.utilities.data import to_onehot
 from torchmetrics.segmentation import MeanIoU, GeneralizedDiceScore
+from torchvision import transforms
 import torch
-from torch.nn import functional as F
 from torch import nn as nn
-import albumentations as albu
-from albumentations.pytorch.transforms import ToTensorV2
-from src.data.hrcwhu_datamodule import HRCWHUDataModule
+import os
 from src.data.hrcwhu_datamodule import HRCWHU
 from src.models.components.cdnetv1 import CDnetV1
 from src.models.components.cdnetv2 import CDnetV2
@@ -43,14 +38,25 @@ class EvalOnHRCWHU:
             "unetmobv2": UNetMobV2(num_classes=2).to(self.device),
             "kappamask": KappaMask(num_classes=2, in_channels=3).to(self.device),
         }
-        self.__load_weight()
-        self.eval_dataset = self.__load_dataset(root="data/hrcwhu", phase="test")
-        self.model_metrics = {
-            model_name: self.__get_metrics("multiclass", 2, HRCWHU.METAINFO["classes"])
-            for model_name in self.models
+        self.root = "data/hrcwhu"
+        self.model_names_mapping = {
+            "KappaMask": "kappamask",
+            "CDNetv1": "cdnetv1",
+            "CDNetv2": "cdnetv2",
+            "HRCloudNet": "hrcloudnet",
+            "MCDNet": "mcdnet",
+            "SCNN": "scnn",
+            "DBNet": "dbnet",
+            "UNetMobv2": "unetmobv2",
         }
-        self.metrics_loss = {
-            model_name: MeanMetric().to(self.device) for model_name in self.models
+        self.invert_model_mapping = {
+            value: key for key, value in self.model_names_mapping.items()
+        }
+        self.__load_weight()
+        self.data_list = self.__load_data()
+        self.model_metrics = {
+            model_name: self.__get_metrics("multiclass", 2)
+            for model_name in self.model_names_mapping
         }
 
     def __load_weight(self):
@@ -69,50 +75,31 @@ class EvalOnHRCWHU:
             model.load_state_dict(state_dict)
             model.eval()
 
-    def __load_dataset(self, root: str, phase: str):
-        val_pipeline = dict(
-            all_transform=albu.Compose([albu.Resize(256, 256, always_apply=True)]),
-            img_transform=albu.Compose([albu.ToFloat(), ToTensorV2()]),
-            ann_transform=None,
-        )
-        data_module = HRCWHUDataModule(
-            root=root,
-            train_pipeline=val_pipeline,
-            val_pipeline=val_pipeline,
-            test_pipeline=val_pipeline,
-        )
-        data_module.prepare_data()
-        data_module.setup()
-        dataset = data_module.test_dataloader()
-        return dataset
+    def __load_data(self):
+        data_list = []
+        split = "test"
+        split_file = os.path.join(self.root, f"{split}.txt")
+        with open(split_file, "r") as f:
+            for line in f:
+                image_file = line.strip()
+                img_path = os.path.join(self.root, "img_dir", split, image_file)
+                ann_path = os.path.join(self.root, "ann_dir", split, image_file)
+                data_list.append((img_path, ann_path))
+        return data_list
 
-    def __get_metrics(self, task: str, num_classes: int, classes: tuple):
+    def __get_metrics(self, task: str, num_classes: int):
         return MetricCollection(
             {
-                "accuracy": ClasswiseWrapper(
-                    MulticlassAccuracy(num_classes=num_classes, average="none"),
-                    labels=list(classes),
+                "Acc": MulticlassAccuracy(num_classes=num_classes, average="macro"),
+                "Precision": Precision(
+                    task=task, num_classes=num_classes, average="macro"
                 ),
-                "precision": ClasswiseWrapper(
-                    Precision(task=task, num_classes=num_classes, average="none"),
-                    labels=list(classes),
+                "Recall": Recall(task=task, num_classes=num_classes, average="macro"),
+                "F1-Score": F1Score(
+                    task=task, num_classes=num_classes, average="macro"
                 ),
-                "recall": ClasswiseWrapper(
-                    Recall(task=task, num_classes=num_classes, average="none"),
-                    labels=list(classes),
-                ),
-                "f1Score": ClasswiseWrapper(
-                    F1Score(task=task, num_classes=num_classes, average="none"),
-                    labels=list(classes),
-                ),
-                "iou": ClasswiseWrapper(
-                    MeanIoU(num_classes=num_classes, per_class=True),
-                    labels=list(classes),
-                ),
-                "dice": ClasswiseWrapper(
-                    GeneralizedDiceScore(num_classes=num_classes, per_class=True),
-                    labels=list(classes),
-                ),
+                "MIoU": MeanIoU(num_classes=num_classes, per_class=False),
+                "Dice": GeneralizedDiceScore(num_classes=num_classes, per_class=False),
             }
         ).to(self.device)
 
@@ -130,41 +117,6 @@ class EvalOnHRCWHU:
             .astype(np.uint8)
         )
         return mask_colors
-
-    def make_table_and_csv(self, model_name: str, filename="hrc_metrics.csv"):
-        res = {
-            model_name: model_metric.compute()
-            for model_name, model_metric in self.model_metrics.items()
-        }
-        columns = list(res[model_name].keys())
-        width = 50
-        table = Table(title="方法与指标对比")
-        table.add_column(
-            "方法\\指标", justify="center", style="bold", no_wrap=True, width=width
-        )
-        for column in columns:
-            table.add_column(
-                column, justify="center", style="bold", no_wrap=True, width=width
-            )
-        table.add_column(
-            "loss", justify="center", style="bold", no_wrap=True, width=width
-        )
-        csv_data = []
-        csv_data.append(["方法\\指标"] + columns + ["loss"])
-        model_res = {}
-        for method, metrics in res.items():
-            row_data = (
-                [method]
-                + [f"{round(metrics[column].item(),4)}" for column in columns]
-                + [f"{round(self.metrics_loss[method].compute().item(),4)}"]
-            )
-            table.add_row(*row_data)
-            csv_data.append(row_data)
-        with open(filename, "w", newline="", encoding="utf-8") as csvfile:
-            writer = csv.writer(csvfile)
-            writer.writerows(csv_data)
-        Console().print(table)
-        return table
 
     def table_to_csv(self, table: Table, filename="hrc_metrics.csv"):
         # 将Table数据导出到CSV
@@ -195,7 +147,16 @@ class EvalOnHRCWHU:
         # 准备绘图
         draw = ImageDraw.Draw(new_image)
         font = ImageFont.truetype("resource/Arial.ttf", 50)
-        column_titles = ["Original", "gt"] + list(self.models.keys())
+        column_titles = ["Input", "Label"] + [
+            "UNetMobv2",
+            "DBNet",
+            "CDNetv1",
+            "HRCloudNet",
+            "KappaMask",
+            "CDNetv2",
+            "SCNN",
+            "MCDNet",
+        ]
         num_cols = len(column_titles)
         col_width = width // num_cols
 
@@ -210,52 +171,109 @@ class EvalOnHRCWHU:
             y = (50 - text_height) // 2
 
             # 绘制文本
-            draw.text((x, y), title, fill="black", font=font)
+            draw.text((x, y-10), title, fill="black", font=font)
         new_image.save("1.png", dpi=(300, 300))
+        new_image.save("1.pdf",dpi=(300, 300))
+
+    def __get_img_ann(
+        self, img_path: str, ann_path: str
+    ) -> Tuple[torch.Tensor, torch.Tensor]:
+        center_crop = transforms.CenterCrop((256, 256))
+        img_to_tensor = transforms.ToTensor()
+        ann_to_tensor = transforms.PILToTensor()
+        img = Image.open(img_path)
+        ann = Image.open(ann_path)
+
+        img = center_crop(img)
+        img = img_to_tensor(img)
+        img = img.unsqueeze(0)
+
+        ann = center_crop(ann)
+        ann = ann_to_tensor(ann)
+
+        return img.to(self.device), ann.to(self.device)
 
     @torch.no_grad()
     def inference(
         self, img: torch.Tensor, target: torch.Tensor, model: nn.Module
-    ) -> Tuple[torch.Tensor, torch.Tensor]:
+    ) -> torch.Tensor:
         logits = model(img)
         if isinstance(logits, tuple):
             logits = logits[0]
         pred = torch.argmax(logits, 1).detach()
-        loss = F.cross_entropy(logits, target).detach()
-        return pred, loss
+        return pred
+
+    def make_table_and_csv(self, filename="hrc_metrics.csv"):
+        data = {
+            model_name: model_metric.compute()
+            for model_name, model_metric in self.model_metrics.items()
+        }
+        metric_weights = {
+            "Acc": 0.2,
+            "Dice": 0.2,
+            "F1-Score": 0.2,
+            "MIoU": 0.2,
+            "Precision": 0.1,
+            "Recall": 0.1,
+        }
+        averages = {
+            model: sum(
+                value[metric].item() * metric_weights[metric] for metric in value
+            )
+            for model, value in data.items()
+        }
+        sorted_models = sorted(averages, key=averages.get)
+        correct_flow = ["Acc", "Precision", "Recall", "F1-Score", "MIoU", "Dice"]
+        with open("tmp.csv", "w", newline="") as csvfile:
+            fieldnames = ["Method"] + list(next(iter(data.values())).keys())
+            writer = csv.DictWriter(csvfile, fieldnames=fieldnames)
+
+            writer.writeheader()
+            for model in sorted_models:
+                row = {"Method": model}
+                row_data = {
+                    metric: round(data[model][metric].item(), 4)
+                    for metric in correct_flow
+                }
+                row.update(row_data)
+                writer.writerow(row)
 
     def run(self):
         """
         评测模型
         """
+        model_order = [
+            "UNetMobv2",
+            "DBNet",
+            "CDNetv1",
+            "HRCloudNet",
+            "KappaMask",
+            "CDNetv2",
+            "SCNN",
+            "MCDNet",
+        ]
         show_images = None
-        for data in track(
-            self.eval_dataset, description="evaling...", total=len(self.eval_dataset)
+        for img_path, ann_path in track(
+            self.data_list, description="evaling...", total=len(self.data_list)
         ):
-            img = data["img"].to(self.device)
-            ann = data["ann"].to(self.device)
-            img_path = data["img_path"][0]
-            # 计算各种指标
-            masks = []
+            img, ann = self.__get_img_ann(img_path, ann_path)
+            model_masks = {}
             for model_name, model in self.models.items():
-                pred, loss = self.inference(img, ann, model)
-                self.model_metrics[model_name].update(
-                    to_onehot(pred, num_classes=2), to_onehot(ann, num_classes=2)
-                )
-                self.metrics_loss[model_name].update(loss)
+                model_name = self.invert_model_mapping[model_name]
+                pred = self.inference(img, ann, model)
+                self.model_metrics[model_name].update(pred, ann)
                 color_mask = self.give_colors_to_mask(img[0], pred)
-                masks.append(color_mask)
-
+                model_masks[model_name] = color_mask
             image = img[0].detach().cpu().permute(1, 2, 0).numpy()
             gt = self.give_colors_to_mask(img[0], ann)
+            masks = [model_masks[mask_name] for mask_name in model_order]
             masks = [image] + [gt] + masks
             masks = np.array(masks)
             if show_images is None:
                 show_images = masks
             else:
                 show_images = np.concatenate((show_images, masks), axis=0)
-        self.make_table_and_csv(model_name)
-
+        # self.make_table_and_csv()
         self.visualize_img(show_images)
 
 
