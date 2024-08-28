c3b6d9fd8b8b3dc2a0d68af263b5d7c085b4213d
caixiaoshun <3038523973@qq.com>
2024-08-28 18:57:22 +0800

    l8_biome的dataset,dataloader完毕

---
+++ src/data/components/l8_biome.py
@@ -0,0 +1,131 @@
+import os
+
+import albumentations
+import numpy as np
+from glob import glob
+from PIL import Image
+from torch.utils.data import Dataset
+from natsort import natsorted
+from typing import List, Literal
+
+
+class L8_BIOME(Dataset):
+    METAINFO = dict(
+        classes=("fill", "cloud shadow", "clear", "thin cloud", "cloud"),
+        palette=(
+            (31, 119, 180),
+            (255, 127, 14),
+            (44, 160, 44),
+            (214, 39, 40),
+            (148, 103, 189),
+        ),
+        img_size=(512, 512),  # H, W
+        ann_size=(512, 512),  # H, W
+    )
+
+    def __init__(
+        self,
+        root: str = None,
+        bands: List[str] = ["2", "3", "4"],
+        phase: Literal["train", "test", "val"] = "train",
+        all_transform: albumentations.Compose = None,
+        img_transform: albumentations.Compose = None,
+        ann_transform: albumentations.Compose = None,
+    ):
+        self.root = root
+        self.phase = phase
+        self.bands = bands
+        self.all_transform = all_transform
+        self.img_transform = img_transform
+        self.ann_transform = ann_transform
+        self.data = self.load_data()
+
+    def load_data(self):
+        dirs = os.listdir(os.path.join(self.root, "img_crop"))
+        dirs = natsorted(dirs)
+        length = len(dirs)
+
+        ratio = 0.8
+        if self.phase == "train":
+            dirs = dirs[: int(length * ratio)]
+        elif self.phase == "val":
+            dirs = dirs[int(length * ratio) :]
+        elif self.phase == "test":
+            dirs = dirs[int(length * ratio) :]
+        else:
+            raise ValueError(
+                "phase must be train, val or test,but got {}".format(self.phase)
+            )
+
+        assert len(dirs) > 0, "No data found in {}".format(self.root)
+        images_path = os.path.join(
+            self.root, "img_crop", dirs[0], f"{dirs[0]}*_B{self.bands[0]}*.tif"
+        )
+
+        file_indexs = []
+        for image_path in glob(images_path):
+            filename = image_path.split(os.path.sep)[-2]
+            index = (
+                image_path.split(os.path.sep)[-1]
+                .split(filename)[-1]
+                .split(".")[0]
+                .split(f"B{self.bands[0]}_")[-1]
+            )
+            file_indexs.append(index)
+        data_list = []
+        for dir in dirs:
+            img_path = os.path.join(self.root, "img_crop", dir)
+            filename = dir
+            for index in file_indexs:
+                data_list.append([img_path, filename, index])
+        return data_list
+
+    def __len__(self):
+        return len(self.data)
+
+    def __get_img_ann(self, img_path: str, filename: str, index: str):
+        image = None
+        for bands in self.bands:
+            image_path = os.path.join(
+                img_path, f"{filename}_B{bands[0]}_{index}.tif"
+            )
+            im = np.array(Image.open(image_path))[:,:,np.newaxis]
+            if image is None:
+                image = im
+            else:
+                image = np.concatenate((image, im), axis=-1)
+
+        ann_path = os.path.join(
+            img_path, f"{filename}_fixedmask_{index}.tif"
+        ).replace("img_crop", "seg_crop")
+
+        ann = np.array(Image.open(ann_path))
+        return image, ann
+
+    def __getitem__(self, idx):
+        img_path, filename, index = self.data[idx]
+        img, ann = self.__get_img_ann(img_path, filename, index)
+
+        if self.all_transform:
+            albumention = self.all_transform(image=img, mask=ann)
+            img = albumention["image"]
+            ann = albumention["mask"]
+
+        if self.img_transform:
+            img = self.img_transform(image=img)["image"]
+
+        if self.ann_transform:
+            ann = self.ann_transform(image=img)["image"]
+
+        return {
+            "img": img,
+            "ann": np.int64(ann),
+            "img_path": filename,
+        }
+
+
+if __name__ == "__main__":
+    root = "/data/zouxuechao/cloudseg/l8_biome/BC"
+    dataset = L8_BIOME(root=root)
+    data = dataset[0]
+    print(data['img'].shape,data['ann'].shape)
---
+++ src/data/components/l8_biome_datamodule.py
@@ -0,0 +1,86 @@
+from typing import Any, Dict, Optional, List
+
+from lightning import LightningDataModule
+from torch.utils.data import DataLoader, Dataset
+
+from src.data.components.l8_biome import L8_BIOME
+from src.data.base_datamodule import BaseDataModule
+
+
+class L8_BIOMEDataModule(BaseDataModule):
+    def __init__(
+        self,
+        root: str,
+        train_pipeline: None,
+        val_pipeline: None,
+        test_pipeline: None,
+        batch_size: int = 1,
+        num_workers: int = 0,
+        pin_memory: bool = False,
+        persistent_workers: bool = False,
+        bands: List[str] = ["2", "3", "4"],
+    ) -> None:
+        super().__init__(
+            root=root,
+            train_pipeline=train_pipeline,
+            val_pipeline=val_pipeline,
+            test_pipeline=test_pipeline,
+            batch_size=batch_size,
+            num_workers=num_workers,
+            pin_memory=pin_memory,
+            persistent_workers=persistent_workers,
+            bands=bands
+        )
+
+
+    @property
+    def num_classes(self) -> int:
+        return len(L8_BIOME.METAINFO["classes"])
+
+    def prepare_data(self) -> None:
+        # train
+        L8_BIOME(
+            root=self.hparams.root,
+            phase="train",
+            bands=self.hparams.bands,
+            **self.hparams.train_pipeline,
+        )
+        
+        # val or test
+        L8_BIOME(
+            root=self.hparams.root,
+            phase="test",
+            bands=self.hparams.bands,
+            **self.hparams.test_pipeline,
+        )
+
+    def setup(self, stage: Optional[str] = None) -> None:
+        # Divide batch size by the number of devices.
+        if self.trainer is not None:
+            if self.hparams.batch_size % self.trainer.world_size != 0:
+                raise RuntimeError(
+                    f"Batch size ({self.hparams.batch_size}) is not divisible by the number of devices ({self.trainer.world_size})."
+                )
+            self.batch_size_per_device = self.hparams.batch_size // self.trainer.world_size
+
+        # load and split datasets only if not loaded already
+        if not self.train_dataset and not self.val_dataset and not self.test_dataset:
+            self.train_dataset = L8_BIOME(
+                root=self.hparams.root,
+                bands=self.hparams.bands,
+                phase="train",
+                **self.hparams.train_pipeline,
+            )
+            
+            self.val_dataset = self.test_dataset = L8_BIOME(
+                root=self.hparams.root,
+                bands=self.hparams.bands,
+                phase="test",
+                **self.hparams.test_pipeline,
+            )
+
+    
+
+
+if __name__ == "__main__":
+    _ = L8_BIOMEDataModule()
