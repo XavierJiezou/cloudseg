2a749e913566c5e5e1ca66c69c3628c6f557f4f7
XavierJiezou <xuechaozou@foxmail.com>
2024-09-10 16:18:11 +0800

    logger改为tensorboard

--- scripts/schedule.sh
+++ scripts/schedule.sh
@@ -2,6 +2,9 @@
 # Schedule execution of many runs
 # Run from root folder with: bash scripts/schedule.sh
 
-python src/train.py trainer.max_epochs=5 logger=csv
-
-python src/train.py trainer.max_epochs=10 logger=csv
+python src/train.py experiment=hrc_whu/scnn.yaml trainer.devices=[6] trainer.max_epochs=1
+python src/train.py experiment=cloudsen12_high_l1c/scnn.yaml trainer.devices=[6] trainer.max_epochs=1
+python src/train.py experiment=cloudsen12_high_l2a/scnn.yaml trainer.devices=[6] trainer.max_epochs=1
+python src/train.py experiment=l8_biome/scnn.yaml trainer.devices=[6] trainer.max_epochs=1
+python src/train.py experiment=gf12ms_whu_gf1/scnn.yaml trainer.devices=[6] trainer.max_epochs=1
+python src/train.py experiment=gf12ms_whu_gf2/scnn.yaml trainer.devices=[6] trainer.max_epochs=1
